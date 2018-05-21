831ff6c8b20db0dfbae6d895bbe8daa98fefe6ea
Braatz, Jonathan <Jonathan.Braatz@arcesium.com>
2018-05-21 11:18:55 -0400

    update

---
+++ .xmonad/haxorCode.py
@@ -0,0 +1,12 @@
+#define ROOTPASSWORD 6969
+
+// TODO: fix this
+Code = program.docx
+run_program(Code, arguments=cmd.exe)
+
+sort
+
+Checklist:
+	Hack Mainframe
+	Dump core, save to disk, and get on outta here
+
